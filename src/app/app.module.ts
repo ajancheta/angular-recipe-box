26836a6c30f87b95e4027be3960f30dcb1183f28
aj ancheta and stuart mckay <aj+stuart@>
2019-06-11 12:01:21 -0700

    add functionality to display hard coded recipes in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-import { NavComponentComponent } from './nav-component/nav-component.component';
-import { ThingComponent } from './thing/thing.component';
 import { RecipeComponent } from './recipe/recipe.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponentComponent,
-    ThingComponent,
     RecipeComponent
   ],
   imports: [
--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../models/recipe.model';
+import { Component } from '@angular/core';
+import { Recipe } from './../models/recipe.model';
 
 @Component({
   selector: 'app-recipe',
@@ -7,16 +7,14 @@ import { Recipe } from '../models/recipe.model';
   styleUrls: ['./recipe.component.css']
 })
 
-export class RecipeComponent implements OnInit {
+export class RecipeComponent {
 
-  constructor() { }
+  // constructor() { }
 
-  ngOnInit() {
-  }
 
-  currentItem: string = 'Explore Our Recipes';
+  title: string = 'Explore Our Recipes';
 
-  recipe: Recipe[] = [
+  recipes: Recipe[] = [
     new Recipe('Cereal', ['Wheaties Cereal', 'Pea Milk'], ['Pour milk into the cereal box', 'Next pour in your Wheaties cereal in the bowl']),
     new Recipe('Ramen', ['Top Ramen', 'Flavor packet', 'Hot water'], ['Drink boiling water', 'Snort the provided flavor packet', 'Eat the dry ramen noodles']),
     new Recipe('Buttered Tortilla', ['Torilla', 'I Can\'t Believe It\'s Not Butter - Pump Bottle'], ['Remove the CPU cooling fan from your home machine and replace with tortilla', 'Start playing Counter Strike to get your machine warmed up', 'After about 10 hours flip tortilla and then wait another 10 hours or until crispy', 'Spray the butter on each side until soggy'])
